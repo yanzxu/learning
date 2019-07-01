b60a7a29ebfb492f3d0cca819db44558cf65eefe
yanzxu <yzxu@thoughtworks.com>
2019-07-01 17:30:17 +0800

    add testController

---
+++ src/main/java/com/spring/multiRepo/TestController.java
@@ -0,0 +1,20 @@
+package com.spring.multiRepo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//@RestController
+@Controller
+public class TestController {
+    @RequestMapping(path = "/multiRepo", method = RequestMethod.GET)
+    public ResponseEntity testMultiRepo(){
+        return ResponseEntity.ok("==== success ====");
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public ResponseEntity test(){
+        return ResponseEntity.ok("==== fail ====");
+    }
+}
