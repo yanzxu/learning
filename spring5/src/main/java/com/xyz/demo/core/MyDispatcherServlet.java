8db37a183120598a89a43db0d7a4c6bdbc5abd51
yanzxu <yzxu@thoughtworks.com>
2019-11-24 14:36:03 +0800

    version2

--- spring5/src/main/java/com/xyz/demo/core/MyDispatcherServlet.java
+++ spring5/src/main/java/com/xyz/demo/core/MyDispatcherServlet.java
@@ -17,13 +17,16 @@ import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class MyDispatcherServlet extends HttpServlet {
-    private Map<String, Object> resourceHolder = new HashMap<>();
+    // 用于保存类的实例
+    private Map<String, Object> iocContainer = new HashMap<>();
+    // 用于保存url和method的映射关系
+    private Map<String, Object> handlerMapping = new HashMap<>();
+    private Properties properties = new Properties();
+    private List<String> classNames = new ArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -33,116 +36,79 @@ public class MyDispatcherServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            doDispatch(req, resp);
+            // 委派模式
+            dispatchRequest(req, resp);
         } catch (Exception e) {
             resp.getWriter().write("500 Exception " + Arrays.toString(e.getStackTrace()));
         }
     }
 
-    private void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    private void dispatchRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String url = request.getRequestURI().replace(request.getContextPath(), "").replaceAll("/+", "/");
 
-        if (!resourceHolder.containsKey(url)) {
+        if (!iocContainer.containsKey(url)) {
             response.getWriter().write("404 not found: " + url);
             return;
         }
 
-        Method method = (Method) resourceHolder.get(url);
+        Method method = (Method) handlerMapping.get(url);
 
         Map<String, String[]> params = request.getParameterMap();
-        method.invoke(resourceHolder.get(method.getDeclaringClass().getName()),
-                request, response, params.get("name")[0]);
+        final String beanName = toLowerFistCase(method.getDeclaringClass().getSimpleName());
+
+        System.out.println(String.format("===== invoke method: %s =====", method.getName()));
+        // 利用反射进行动态委派
+        method.invoke(iocContainer.get(beanName), request, response, params.get("name")[0]);
     }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        InputStream inputStream = null;
+        loadConfigure(config.getInitParameter("contextConfigLocation"));
 
-        try {
-            Properties properties = new Properties();
-            inputStream = this.getClass().getClassLoader().getResourceAsStream(config.getInitParameter("contextConfigLocation"));
-            properties.load(inputStream);
-            String scanPackage = properties.getProperty("scanPackage");
+        scanPackages(properties.getProperty("scanPackage"));
 
-            scanPackages(scanPackage);
+        initInstances();
 
-            for (String className : resourceHolder.keySet()) {
-                if (!className.contains(".")) {
-                    continue;
+        autowireInstances();
+
+        initHandlerMapping();
+
+        System.out.println("===== Init spring framework successfully =====");
+    }
+
+    private void initHandlerMapping() {
+        // 策略模式
+        iocContainer.values().stream().map(Object::getClass).filter(clazz -> clazz.isAnnotationPresent(MyController.class))
+                .forEach(clazz -> {
+                    String baseUrl = clazz.isAnnotationPresent(MyRequestMapping.class) ? clazz.getAnnotation(MyRequestMapping.class).value() : "";
+
+                    Stream.of(clazz.getMethods()).filter(method -> method.isAnnotationPresent(MyRequestMapping.class))
+                            .forEach(method -> {
+                                String url = ("/" + baseUrl + "/" + method.getAnnotation(MyRequestMapping.class).value()).replaceAll("/+", "/");
+                                handlerMapping.put(url, method);
+                                System.out.println(String.format("===== put url: %s and method : %s into handlerMapping", url, method));
+                            });
+                });
+    }
+
+    private void autowireInstances() {
+        iocContainer.entrySet().forEach(entry -> {
+            final Field[] declaredFields = entry.getValue().getClass().getDeclaredFields();
+            Stream.of(declaredFields).filter(field -> field.isAnnotationPresent(MyAutowired.class)).forEach(field -> {
+                String beanName = field.getAnnotation(MyAutowired.class).value();
+                if (StringUtils.isEmpty(beanName)) {
+                    beanName = field.getType().getName();
                 }
 
-                Class<?> clazz = Class.forName(className);
-                if (clazz.isAnnotationPresent(MyController.class)) {
-                    resourceHolder.put(className, clazz.newInstance());
-                    String baseUrl = "";
-                    if (clazz.isAnnotationPresent(MyRequestMapping.class)) {
-                        baseUrl = clazz.getAnnotation(MyRequestMapping.class).value();
-                    }
-
-                    Method[] methods = clazz.getMethods();
-                    for (Method method : methods) {
-                        if (!method.isAnnotationPresent(MyRequestMapping.class)) {
-                            continue;
-                        }
-
-                        String url = (baseUrl + "/" + method.getAnnotation(MyRequestMapping.class).value()).replaceAll("/+", "/");
-                        resourceHolder.put(url, method);
-                        System.out.println("==== add resource to holder, key: " + url + "  value: " + method);
-                    }
-                } else if (clazz.isAnnotationPresent(MyService.class)) {
-                    String beanName = clazz.getAnnotation(MyService.class).value();
-                    if (StringUtils.isEmpty(beanName)) {
-                        beanName = clazz.getName();
-                    }
-
-                    Object instance = clazz.newInstance();
-                    resourceHolder.put(beanName, inputStream);
-
-                    for (Class<?> i : clazz.getInterfaces()) {
-                        resourceHolder.put(i.getName(), instance);
-                    }
-                }
-            }
-
-            for (Object object : resourceHolder.values()) {
-                if (object == null) {
-                    continue;
-                }
-
-                Class<?> clazz = object.getClass();
-                if (clazz.isAnnotationPresent(MyController.class)) {
-                    Field[] fields = clazz.getDeclaredFields();
-                    for (Field field : fields) {
-                        if (!field.isAnnotationPresent(MyAutowired.class)) {
-                            continue;
-                        }
-
-                        String beanName = field.getAnnotation(MyAutowired.class).value();
-                        if (StringUtils.isEmpty(beanName)) {
-                            beanName = field.getType().getName();
-                        }
-
-                        field.setAccessible(true);
-                        try {
-                            field.set(resourceHolder.get(clazz.getName()), resourceHolder.get(beanName));
-                        } catch (IllegalAccessException e) {
-                            System.out.println("Exception: 自动注入bean失败，beanName: " + beanName);
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Error: init dispatcher失败");
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null) {
+                field.setAccessible(true);
                 try {
-                    inputStream.close();
-                } catch (IOException e) {
+                    field.set(entry.getValue(), iocContainer.get(beanName));
+                    System.out.println(String.format("===== autowired beanName %s to filed: %s =====", beanName, field.getName()));
+                } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
-            }
-        }
+            });
+        });
     }
 
     private void scanPackages(String packagePath) {
@@ -155,9 +121,52 @@ public class MyDispatcherServlet extends HttpServlet {
             } else {
                 if (file.getName().endsWith(".class")) {
                     String className = packagePath + "." + file.getName().replace(".class", "");
-                    resourceHolder.put(className, null);
+                    classNames.add(className);
+                    System.out.println(String.format("===== add className : %s to classNames", className));
                 }
             }
         }
+    }
+
+    private void loadConfigure(String contextConfigLocation) {
+        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(contextConfigLocation)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initInstances() {
+        // 工厂模式
+        classNames.stream().forEach(className -> {
+            try {
+                Class<?> clazz = Class.forName(className);
+                final Object instance = clazz.newInstance();
+                iocContainer.put(getBeanName(clazz), instance);
+                Stream.of(clazz.getInterfaces()).forEach(i -> iocContainer.put(i.getName(), instance));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private String toLowerFistCase(String name) {
+        final char[] chars = name.toCharArray();
+        chars[0] += 32;
+        return String.valueOf(chars);
+    }
+
+    private String getBeanName(Class<?> clazz) {
+        if (clazz.isAnnotationPresent(MyController.class)) {
+            return StringUtils.isEmpty(clazz.getAnnotation(MyController.class).value()) ? toLowerFistCase(clazz.getSimpleName())
+                    : clazz.getAnnotation(MyController.class).value();
+        }
+
+        if (clazz.isAnnotationPresent(MyService.class)) {
+            return StringUtils.isEmpty(clazz.getAnnotation(MyService.class).value()) ? toLowerFistCase(clazz.getSimpleName())
+                    : clazz.getAnnotation(MyService.class).value();
+        }
+
+        return "";
     }
 }
