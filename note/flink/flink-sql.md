900d3ddb8384a9248a6663fe83fa66a289c02a2f
yanzxu <yzxu@thoughtworks.com>
2020-10-02 10:26:59 +0800

    note for flink

---
+++ note/flink/flink-sql.md
@@ -0,0 +1,5 @@
+# Table API 和 Flink SQL
+
+- Flink对批处理和流处理，提供了统一的上层API；
+- Table API是一套内嵌在java和Scala语言中的查询API，他允许以非常直观的方式组合来自一些关系运算符的查询；
+- Flink的SQL支持基于实现了Sql标准的Apache Calcite；
---
+++ src/main/java/com/jvm/classloader/ClinitTest.java
@@ -0,0 +1,17 @@
+package com.jvm.classloader;
+
+public class ClinitTest {
+
+    public static void main(String[] args) {
+        int i =0;
+        long str = 10l;
+
+    }
+
+    public static void test(){
+        int num = 10;
+        double d = 2.2;
+
+    }
+
+}
